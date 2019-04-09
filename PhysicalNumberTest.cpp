3f84309318024d771e77a2dc5ed151acb163bfb1
ofrahadad <44833571+ofraHadad@users.noreply.github.com>
2019-04-09 22:38:41 +0300

    first step

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -23,11 +23,24 @@ int main() {
     PhysicalNumber b(300, Unit::M);
     PhysicalNumber c(2, Unit::HOUR);
     PhysicalNumber d(30, Unit::MIN);
+    PhysicalNumber e(100, Unit::CM);
+    PhysicalNumber f(30, Unit::SEC);
+    PhysicalNumber g(5, Unit::TON);
+    PhysicalNumber h(100, Unit::KG);
+    PhysicalNumber i(15, Unit::G);
+
 
     testcase
     .setname("Basic output")
     .CHECK_OUTPUT(a, "2[km]")
     .CHECK_OUTPUT(b, "300[m]")
+    .CHECK_OUTPUT(c, "2[hour]")
+    .CHECK_OUTPUT(d, "30[min]")
+    .CHECK_OUTPUT(e, "100[cm]")
+    .CHECK_OUTPUT(f, "30[sec]")
+    .CHECK_OUTPUT(g, "5[ton]")
+    .CHECK_OUTPUT(h, "100[kg]")
+    .CHECK_OUTPUT(i, "15[g]")
 
     .setname("Compatible dimensions")
     .CHECK_OUTPUT(b+a, "2300[m]")
@@ -38,18 +51,39 @@ int main() {
     .CHECK_OUTPUT(c, "2[hour]")
     .CHECK_OUTPUT(d, "30[min]")
     .CHECK_OUTPUT(d+c, "150[min]")
+    .CHECK_OUTPUT(a+e, "2.001[km]")
+    .CHECK_OUTPUT(d+f, "30.5[min]")
+    .CHECK_OUTPUT(g+h, "5.1[ton]")
+   .CHECK_OUTPUT(i+g, "5000015[g]")
+   .CHECK_EQUAL(i==g, false)
+   .CHECK_EQUAL(i!=g, true)
+   .CHECK_EQUAL(d==d, true)
+  .CHECK_EQUAL(a>=b, true)
+  .CHECK_EQUAL(i>=g, false)
+  .CHECK_EQUAL(d<f, false)
+  .CHECK_OUTPUT(++a, "2[km]")
+  .CHECK_OUTPUT(i++, "16[g]")
+   .CHECK_OUTPUT(-e, "-100[cm]")
+  .CHECK_OUTPUT(+d, "30[min]")
+
+
+
 
     .setname("Incompatible dimensions")
     .CHECK_THROWS(a+c)
     .CHECK_THROWS(a+d)
     .CHECK_THROWS(b+c)
     .CHECK_THROWS(b+d)
+   .CHECK_THROWS(g==f)
+   .CHECK_THROWS(e!=d)
+   .CHECK_THROWS(b+d)
+   .CHECK_THROWS(g<=a)
 
     .setname("Basic input")
     .CHECK_OK(istringstream("700[kg]") >> a)
     .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
 
-    // YOUR TESTS - INSERT AS MANY AS YOU WANT
+    
 
       .setname("...")
 
@@ -59,6 +93,6 @@ int main() {
       testcase.print_signal(signal);
       grade = 0;
     }
-    cout <<  "*** Grade: " << grade << " ***" << endl;
+    cout <<  "* Grade: " << grade << " *" << endl;
     return grade;
 }
